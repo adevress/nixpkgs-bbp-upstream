c5430176997a79e854de8da7274ca4abf36fe5a9
Adrien Devresse <adrien.devresse@epfl.ch>
2016-05-20 11:20:34 +0200

    Swithc the bootstrap source to external server

--- pkgs/stdenv/linux/bootstrap/powerpc64.nix
+++ pkgs/stdenv/linux/bootstrap/powerpc64.nix
@@ -1,12 +1,13 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = http://bbpcf011.epfl.ch/others/release-2/busybox;
-    sha256 = "00b9s8d9nxv87zgcm4pkdizlvqxck6mwahcndi7xyjv0lg5bbsq5";
+    url = http://adev.name/bootstrap/powerpc64/r2/busybox;
+    sha256 = "0qkc5k31n7c62a7mpf0dyq31sixsab52dkxfgdbmp5w4aw541rx2";
     executable = true;
   };
 
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = http://bbpcf011.epfl.ch/others/release-2/bootstrap-tools.tar.xz;   
+
+    url = http://adev.name/bootstrap/powerpc64/r2/bootstrap-tools.tar.xz;   
     sha256 = "0b0lkhvdflb209cwwzxm0bcbz0inq1awk22d83jdrz56h9xmrc9v";
   };
 }
