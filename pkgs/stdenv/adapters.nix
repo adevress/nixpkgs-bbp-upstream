97b1e06d27ea78262dc52044783c0611b2d871fc
Adrien Devresse <adrien.devresse@epfl.ch>
2016-08-10 13:28:42 +0200

    Fix issue with cross compilation  in makeStdenvCross

--- pkgs/stdenv/adapters.nix
+++ pkgs/stdenv/adapters.nix
@@ -56,66 +56,75 @@ rec {
 
   # Return a modified stdenv that adds a cross compiler to the
   # builds.
-  makeStdenvCross = stdenv: cross: binutilsCross: gccCross: stdenv //
-    { mkDerivation = {name ? "", buildInputs ? [], nativeBuildInputs ? [],
-            propagatedBuildInputs ? [], propagatedNativeBuildInputs ? [],
-            selfNativeBuildInput ? false, ...}@args: let
+  makeStdenvCross = stdenv: cross: binutilsCross: gccCross: 
+	let 
+	    result = stdenv //
+	    { mkDerivation = {name ? "", buildInputs ? [], nativeBuildInputs ? [],
+		    propagatedBuildInputs ? [], propagatedNativeBuildInputs ? [],
+		    selfNativeBuildInput ? false, passthru ? {}, ...}@args: let
 
-            # *BuildInputs exists temporarily as another name for
-            # *HostInputs.
+		    # *BuildInputs exists temporarily as another name for
+		    # *HostInputs.
 
-            # In nixpkgs, sometimes 'null' gets in as a buildInputs element,
-            # and we handle that through isAttrs.
-            getNativeDrv = drv: drv.nativeDrv or drv;
-            getCrossDrv = drv: drv.crossDrv or drv;
-            nativeBuildInputsDrvs = map getNativeDrv nativeBuildInputs;
-            buildInputsDrvs = map getCrossDrv buildInputs;
-            buildInputsDrvsAsBuildInputs = map getNativeDrv buildInputs;
-            propagatedBuildInputsDrvs = map getCrossDrv propagatedBuildInputs;
-            propagatedNativeBuildInputsDrvs = map getNativeDrv propagatedNativeBuildInputs;
+		    # In nixpkgs, sometimes 'null' gets in as a buildInputs element,
+		    # and we handle that through isAttrs.
+		    getNativeDrv = drv: drv.nativeDrv or drv;
+		    getCrossDrv = drv: drv.crossDrv or drv;
+		    nativeBuildInputsDrvs = map getNativeDrv nativeBuildInputs;
+		    buildInputsDrvs = map getCrossDrv buildInputs;
+		    buildInputsDrvsAsBuildInputs = map getNativeDrv buildInputs;
+		    propagatedBuildInputsDrvs = map getCrossDrv propagatedBuildInputs;
+		    propagatedNativeBuildInputsDrvs = map getNativeDrv propagatedNativeBuildInputs;
 
-            # The base stdenv already knows that nativeBuildInputs and
-            # buildInputs should be built with the usual gcc-wrapper
-            # And the same for propagatedBuildInputs.
-            nativeDrv = stdenv.mkDerivation args;
+		    # The base stdenv already knows that nativeBuildInputs and
+		    # buildInputs should be built with the usual gcc-wrapper
+		    # And the same for propagatedBuildInputs.
+		    nativeDrv = stdenv.mkDerivation args;
 
-            # Temporary expression until the cross_renaming, to handle the
-            # case of pkgconfig given as buildInput, but to be used as
-            # nativeBuildInput.
-            hostAsNativeDrv = drv:
-                builtins.unsafeDiscardStringContext drv.nativeDrv.drvPath
-                == builtins.unsafeDiscardStringContext drv.crossDrv.drvPath;
-            buildInputsNotNull = stdenv.lib.filter
-                (drv: builtins.isAttrs drv && drv ? nativeDrv) buildInputs;
-            nativeInputsFromBuildInputs = stdenv.lib.filter hostAsNativeDrv buildInputsNotNull;
+		    # Temporary expression until the cross_renaming, to handle the
+		    # case of pkgconfig given as buildInput, but to be used as
+		    # nativeBuildInput.
+		    hostAsNativeDrv = drv:
+			builtins.unsafeDiscardStringContext drv.nativeDrv.drvPath
+			== builtins.unsafeDiscardStringContext drv.crossDrv.drvPath;
+		    buildInputsNotNull = stdenv.lib.filter
+			(drv: builtins.isAttrs drv && drv ? nativeDrv) buildInputs;
+		    nativeInputsFromBuildInputs = stdenv.lib.filter hostAsNativeDrv buildInputsNotNull;
 
-            # We should overwrite the input attributes in crossDrv, to overwrite
-            # the defaults for only-native builds in the base stdenv
-            crossDrv = if cross == null then nativeDrv else
-                stdenv.mkDerivation (args // {
-                    name = name + "-" + cross.config;
-                    nativeBuildInputs = nativeBuildInputsDrvs
-                      ++ nativeInputsFromBuildInputs
-                      ++ [ gccCross binutilsCross ] ++
-                      stdenv.lib.optional selfNativeBuildInput nativeDrv;
 
-                    # Cross-linking dynamic libraries, every buildInput should
-                    # be propagated because ld needs the -rpath-link to find
-                    # any library needed to link the program dynamically at
-                    # loader time. ld(1) explains it.
-                    buildInputs = [];
-                    propagatedBuildInputs = propagatedBuildInputsDrvs ++ buildInputsDrvs;
-                    propagatedNativeBuildInputs = propagatedNativeBuildInputsDrvs;
+		    # We should overwrite the input attributes in crossDrv, to overwrite
+		    # the defaults for only-native builds in the base stdenv
+		    crossDrv = if cross == null then nativeDrv else
+			stdenv.mkDerivation (args // {
+			    name = name + "-" + cross.config;
+			    nativeBuildInputs = nativeBuildInputsDrvs
+			      ++ nativeInputsFromBuildInputs
+			      ++ [ gccCross binutilsCross ] ++
+			      stdenv.lib.optional selfNativeBuildInput nativeDrv;
 
-                    crossConfig = cross.config;
-                } // args.crossAttrs or {});
-        in nativeDrv // {
-          inherit crossDrv nativeDrv;
-        };
-    } // {
-      inherit cross gccCross binutilsCross;
-      ccCross = gccCross;
-    };
+			    # Cross-linking dynamic libraries, every buildInput should
+			    # be propagated because ld needs the -rpath-link to find
+			    # any library needed to link the program dynamically at
+			    # loader time. ld(1) explains it.
+			    buildInputs = [];
+			    propagatedBuildInputs = propagatedBuildInputsDrvs ++ buildInputsDrvs;
+			    propagatedNativeBuildInputs = propagatedNativeBuildInputsDrvs;
+
+			    crossConfig = cross.config;
+
+
+		    	    passthru = passthru // { stdenv = result; };
+
+			} // args.crossAttrs or {});
+		in nativeDrv // {
+		  inherit crossDrv nativeDrv;
+		};
+	    } // {
+	      inherit cross gccCross binutilsCross;
+	      ccCross = gccCross;
+	      
+	    };
+	in result;
 
 
   /* Modify a stdenv so that the specified attributes are added to
