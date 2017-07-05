93f8cf2ad2ee4a35668b10e0315d064b5c5abb57
Adrien Devresse <adrien.devresse@epfl.ch>
2017-07-05 15:30:36 +0200

    Add basic power8 configuration

--- lib/platforms.nix
+++ lib/platforms.nix
@@ -9,16 +9,17 @@ rec {
   i686 = ["i686-linux" "i686-freebsd" "i686-netbsd" "i686-cygwin"];
   mips = [ "mips64el-linux" ];
   x86_64 = ["x86_64-linux" "x86_64-darwin" "x86_64-freebsd" "x86_64-openbsd" "x86_64-netbsd" "x86_64-cygwin"];
+  power = [ "powerpc64le-linux" ];
 
   cygwin = ["i686-cygwin" "x86_64-cygwin"];
   darwin = ["x86_64-darwin"];
   freebsd = ["i686-freebsd" "x86_64-freebsd"];
   gnu = linux; /* ++ hurd ++ kfreebsd ++ ... */
   illumos = ["x86_64-solaris"];
-  linux = ["i686-linux" "x86_64-linux" "armv5tel-linux" "armv6l-linux" "armv7l-linux" "aarch64-linux" "mips64el-linux"];
+  linux = ["i686-linux" "x86_64-linux" "armv5tel-linux" "armv6l-linux" "armv7l-linux" "aarch64-linux" "mips64el-linux" "powerpc64le-linux" ];
   netbsd = ["i686-netbsd" "x86_64-netbsd"];
   openbsd = ["i686-openbsd" "x86_64-openbsd"];
   unix = linux ++ darwin ++ freebsd ++ openbsd ++ netbsd ++ illumos;
 
-  mesaPlatforms = ["i686-linux" "x86_64-linux" "x86_64-darwin" "armv5tel-linux" "armv6l-linux" "armv7l-linux" "aarch64-linux"];
+  mesaPlatforms = ["i686-linux" "x86_64-linux" "x86_64-darwin" "armv5tel-linux" "armv6l-linux" "armv7l-linux" "aarch64-linux" "powerpc64le-linux" ];
 }
